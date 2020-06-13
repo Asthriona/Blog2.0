7fdde2d2bc525fca376b79057490efb48b18f330
Asthriona <im@asthriona.com>
2020-06-13 22:07:09 +0200

    add index router

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var app = express();
 
 //router
 var articleRoute = require('./routes/article');
+var indexRoute = require('./routes/index');
+var backRoute = require('./routes/back');
 
 
 //middleware
@@ -20,16 +22,8 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'))
+app.use('/', indexRoute);
 app.use('/articles', articleRoute);
-
-app.get('/', async (req,res)=>{
-    var articles = await Article.find().sort({
-        createdAt: 'desc'
-    })
-    res.render('index', { articles: articles })
-})
-app.get('/about', async (req,res)=>{
-    res.render('about')
-})
+app.use('/articles', backRoute);
 
 app.listen(config.port, console.log(`Asthriona's blog is now running on http://localhost:${config.port}`));
