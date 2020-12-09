5da0e19b672bf2a5b92ebae3ecbfb1ff1b52f6ab
Asthriona <im@asthriona.com>
2020-12-09 23:50:50 +0100

    Add hidden option in edit and new article

--- routes/back.js
+++ routes/back.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 var Article = require('../models/article');
+
 function isAuthorise(req, res, next) {
     if (req.user) {
         if (req.user.sitePremium != "1") res.redirect('/nope')
@@ -13,10 +14,10 @@ function isAuthorise(req, res, next) {
     }
 }
 
-router.get('/login', async (req,res)=>{
-    res.render('back/login', {title: "Asthriona - Login"})
+router.get('/login', async(req, res) => {
+    res.render('back/login', { title: "Asthriona - Login" })
 });
-router.get('/', isAuthorise, async (req,res)=>{
+router.get('/', isAuthorise, async(req, res) => {
     var articles = await Article.find().sort({
         createdAt: 'desc'
     })
@@ -27,10 +28,10 @@ router.get('/', isAuthorise, async (req,res)=>{
         title: "Asthriona - Admin panel"
     })
 });
-router.get('/new', isAuthorise, (req,res)=>{
-    res.render('articles/new', {article: new Article(), title: "Asthriona - New Post"})
+router.get('/new', isAuthorise, (req, res) => {
+    res.render('articles/new', { article: new Article(), title: "Asthriona - New Post" })
 });
-router.get('/edit/:id', isAuthorise, async (req,res)=>{
+router.get('/edit/:id', isAuthorise, async(req, res) => {
     const article = await Article.findById(req.params.id)
     res.render('articles/edit', {
         article: article,
@@ -39,48 +40,55 @@ router.get('/edit/:id', isAuthorise, async (req,res)=>{
         title: "Asthriona - Edit ⇒ " + article.title
     })
 });
-router.get('/hide/:id', async (req,res)=>{
+router.get('/hide/:id', async(req, res) => {
     const article = await Article.findById(req.params.id)
     article.hidden = true;
     await article.save()
     res.redirect("/back")
 });
-router.get('/unhide/:id', async (req,res)=>{
+router.get('/unhide/:id', async(req, res) => {
     const article = await Article.findById(req.params.id)
     article.hidden = false;
     article.save()
     res.redirect("/back")
 });
-router.post('/', isAuthorise, async (req,res)=>{
-   let article = new Article({
-       title: req.body.title,
-       description: req.body.description,
-       markdown: req.body.markdown,
-       img: req.body.img,
-   });
-   try{
-       article = await article.save()
-       res.redirect(`articles/${article.slug}`)
-   } catch(e){
-       console.log(e)
-    res.render('articles/new', {article: article})
-   }
+router.post('/', isAuthorise, async(req, res) => {
+    if (req.body.hidden) {
+        var hidden = true
+    } else {
+        var hidden = false
+    }
+    console.log(req.body.hidden)
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown,
+        img: req.body.img,
+        hidden: hidden
+    });
+    try {
+        article = await article.save()
+        res.redirect(`articles/${article.slug}`)
+    } catch (e) {
+        console.log(e)
+        res.render('articles/new', { article: article })
+    }
 });
-router.delete('/:id', async (req,res)=>{
-    await Article.findByIdAndDelete({_id: req.params.id});
+router.delete('/:id', async(req, res) => {
+    await Article.findByIdAndDelete({ _id: req.params.id });
     res.redirect('/back');
 });
-async function saveArticleAndRedirect(path){
-    return async (req,res)=>{
+async function saveArticleAndRedirect(path) {
+    return async(req, res) => {
         let article = req.article
-            article.title = req.body.title,
+        article.title = req.body.title,
             article.description = req.body.description,
             article.markdown = req.body.markdown
-        try{
+        try {
             article = await article.save()
             res.redirect(`/articles/${article.slug}`)
-        }catch(e){
-            res.render(`back/${path}`, {article: article})
+        } catch (e) {
+            res.render(`back/${path}`, { article: article })
         }
     }
 }
